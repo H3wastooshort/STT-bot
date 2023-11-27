52a00e86306843cbbc87598e47c18cbf6409566f
zatomos <zatomos@yahoo.com>
2023-11-27 23:10:44 +0100

    now correctly restores views upon start

--- cache_handling.py
+++ cache_handling.py
@@ -12,19 +12,19 @@ with open("config.yaml", "r") as file:
     AUDIO_PATH = Path(config["audio_path"])
     CACHE = Path(config["cache"])
 
-def add_to_cache(id : str, author : discord.Member, date : datetime.datetime, content : str) :
+def add_to_cache(audio_msg_id : str, view_msg_id : str, channel_id : discord.TextChannel, author : discord.Member, date : datetime.datetime, content : str) :
     try :
         #check if cache exists and create it if not
         if not os.path.exists(Path(__file__).parent / AUDIO_PATH / CACHE):
             with open(Path(__file__).parent / AUDIO_PATH / CACHE, "w") as file :
                 data = {}
-                data[id] = {"author" : str(author), "date" : str(date), "content" : content}
+                data[audio_msg_id] = {"channel_id" : str(channel_id), "view_id" : view_msg_id, "author" : str(author), "date" : str(date), "content" : content}
                 json.dump(data, file)
                 file.close()
         else : #if cache exists, add new entry
             with open(Path(__file__).parent / AUDIO_PATH / CACHE, "r") as file:
                 cache = json.load(file)
-                cache[id] = {"author" : str(author), "date" : str(date), "content" : content}
+                cache[audio_msg_id] = {"channel_id" : str(channel_id), "view_id" : view_msg_id, "author" : str(author), "date" : str(date), "content" : content}
                 file.close()
             with open(Path(__file__).parent / AUDIO_PATH / CACHE, "w") as file:
                 json.dump(cache, file)
@@ -33,3 +33,13 @@ def add_to_cache(id : str, author : discord.Member, date : datetime.datetime, co
         logging.error("Error writing to cache: %s", e)
         return False
     return True
+
+def get_all_cache() :
+    try :
+        with open(Path(__file__).parent / AUDIO_PATH / CACHE, "r") as file:
+            cache = json.load(file)
+            file.close()
+    except Exception as e:
+        logging.error("Error reading cache: %s", e)
+        return None
+    return cache
--- speechtotext.py
+++ speechtotext.py
@@ -62,14 +62,14 @@ class ButtonsView(discord.ui.View):
 # DISCORD BOT EVENTS
 @bot.event
 async def on_message(message: discord.Message):
-    logger.info("Message received")
     if message.author == bot.user:
         return
 
+    #check if message is a voice message
     url = str(message.attachments[0]) if message.attachments else ""
     if url and ".ogg" in url:
         logger.info(f"Voice message URL found: {url}")
-        await message.channel.send("", view=ButtonsView(message))
+        view_message = await message.channel.send("", view=ButtonsView(message))
         try:
             opener = urllib.request.URLopener()
             opener.addheader("User-Agent", "Mozilla/5.0")
@@ -78,17 +78,28 @@ async def on_message(message: discord.Message):
             logger.error("Error retrieving file: %s", e)
             traceback.print_exc()
 
+        #transcription and cache
         output = await wt.transcribe()
-        if c_handle.add_to_cache(message.id, message.author, message.created_at, output) :
+        if c_handle.add_to_cache(message.id, view_message.id, message.channel.id, message.author, message.created_at, output) :
             logger.info("Transcription completed")
         else :
             logger.error("Error adding to cache")
 
 @bot.event
-async def on_ready():
-    logger.info("Bot started!")
-    bot.add_view(ButtonsView())
+async def on_ready():    
+    #restores previously created views
+    cache = c_handle.get_all_cache()
+    if cache is not None:
+        logger.info("Restoring views")
+        for key in cache :
+            audio_msg_id = int(key)
+            view_msg_id = int(cache[key]["view_id"])
+            channel_id = int(cache[key]["channel_id"])
+            audio_msg = await bot.get_channel(channel_id).fetch_message(audio_msg_id)
+            bot.add_view(ButtonsView(audio_msg), message_id=view_msg_id)
+    
     await bot.change_presence(activity=discord.Game(name="user audio"))
+    logger.info("Bot started!")
 
 async def setup():
     bot.add_cog(SpeechToText())
