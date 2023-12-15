d924e8ee545becdfe0cd52d043cd3e2dd63ecf0e
zatomos <zatomos@yahoo.com>
2023-12-15 22:20:55 +0100

    Two-pass transcription

--- utils/whisper_transcribe.py
+++ utils/whisper_transcribe.py
@@ -12,25 +12,25 @@ from . import cache_handling as c_handle
 with open(Path(__file__).parent.parent / "config.yaml", "r") as file:
     config = yaml.safe_load(file)
     AUDIO_PATH = Path(config["audio_path"])
-    MODEL = config["whisper_model"]
+    FIRST_MODEL = config["whisper_model_first_pass"]
+    SECOND_MODEL = config["whisper_model_second_pass"]
     LANGUAGE = config["language"]
     
 # Initialize logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def transcribe(message_id : int) :
+def transcribe(message_id : int, model : str = FIRST_MODEL) :
     #load model
     available_models = whisper.available_models()
-    using_model = MODEL
 
     #check if model exists
-    if using_model not in available_models:
-        logger.warning(f"Model {MODEL} not found; using small model instead")
-        using_model = "small"
+    if model not in available_models:
+        logger.warning(f"Model {model} not found; using small model instead")
+        model = "small"
 
     #load model
-    model = whisper.load_model(using_model)
+    model = whisper.load_model(model)
 
     #load audio file
     audio = whisper.load_audio(Path(__file__).parent.parent / AUDIO_PATH / f"voice_message_{str(message_id)}.ogg")
@@ -41,20 +41,20 @@ def transcribe(message_id : int) :
     else :
         result = model.transcribe(audio, language=LANGUAGE)
 
-    #clean up
-    os.remove(Path(__file__).parent.parent / AUDIO_PATH / f"voice_message_{str(message_id)}.ogg")
     return result['text']
 
 def transcribe_and_cache(message : discord.Message, view_message : discord.Message) :
     '''Transcribes the audio file and adds the result to the cache, then removes old entries from the cache'''
 
-    #transcribe
+    #FIRST PASS
+    logger.info("Starting first pass transcription")
+
     try :
-        output = transcribe(message.id)
+        output = transcribe(message.id, FIRST_MODEL)
     except Exception as e:
         logger.error("Error during the transcription: %s", e)
-        
-        #clean up
+
+        #clean up if error
         os.remove(Path(__file__).parent.parent / AUDIO_PATH / f"voice_message_{str(message.id)}.ogg")
         return
     
@@ -65,16 +65,42 @@ def transcribe_and_cache(message : discord.Message, view_message : discord.Messa
         logger.error("Error adding to cache")
     c_handle.remove_old_cache()
 
+    #SECOND PASS
+    logger.info("Starting second pass transcription")
+
+    try :
+        output = transcribe(message.id, SECOND_MODEL)
+    except Exception as e:
+        logger.error("Error during the transcription: %s", e)
+        
+        #clean up if error
+        os.remove(Path(__file__).parent.parent / AUDIO_PATH / f"voice_message_{str(message.id)}.ogg")
+        return
+    
+    #cache
+    if c_handle.add_to_cache(message.id, view_message.id, message.channel.id, message.author, message.created_at, output) :
+        logger.info("Transcription completed")
+    else :
+        logger.error("Error adding to cache")
+    c_handle.remove_old_cache()
+
+    #clean up
+    os.remove(Path(__file__).parent.parent / AUDIO_PATH / f"voice_message_{str(message.id)}.ogg")
+
+
 async def transcribe_no_cache(message_id : int, interaction : discord.Interaction) :
     '''Directly transcribes the audio file and sends the result to the user'''
 
     #transcribe
     try :
-        transcription = await asyncio.to_thread(transcribe, message_id)
+        transcription = await asyncio.to_thread(transcribe, message_id, FIRST_MODEL)
         await interaction.edit_original_response(content=transcription)
     except Exception as e:
         logger.error("Error during the transcription: %s", e)
         
-        #clean up
+        #clean up if error
         await interaction.edit_original_response(content="Error during the transcription")
         os.remove(Path(__file__).parent.parent / AUDIO_PATH / f"voice_message_{str(message_id)}.ogg")
+    
+    #clean up
+    os.remove(Path(__file__).parent.parent / AUDIO_PATH / f"voice_message_{str(message_id)}.ogg")
